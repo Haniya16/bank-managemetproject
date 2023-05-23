b215b0dc1e85e4fe0e0d131f3db9ed141d61d7a8
Haniya16 <mouhammadyounus96@gmail.com>
2023-05-23 15:46:02 +0500

    management

---
+++ bank management/src/BankDetail.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class BankDetail {
+
+    private String accno;
+    private String name;
+    private String acc_type;
+    private long balance;
+    Scanner sc = new Scanner(System.in);
+
+    // method to open new account
+    public void openAccount() {
+        System.out.print("Enter Account No: ");
+        accno = sc.next();
+        System.out.print("Enter Account type: ");
+        acc_type = sc.next();
+        System.out.print("Enter Name: ");
+        name = sc.next();
+        System.out.print("Enter Balance: ");
+        balance = sc.nextLong();
+        System.out.print("\n");
+    }
+
+    // method to display account details
+    public void showAccount() {
+        System.out.println("Name of account holder: " + name);
+        System.out.println("Account no.: " + accno);
+        System.out.println("Account type: " + acc_type);
+        System.out.println("Balance: " + balance);
+        System.out.print("\n");
+    }
+
+    // method to deposit money
+    public void deposit() {
+        long amt;
+        System.out.println("Enter the amount you want to deposit: ");
+        amt = sc.nextLong();
+        balance = balance + amt;
+        System.out.print("\n");
+    }
+
+    // method to withdraw money
+    public void withdrawal() {
+        long amt;
+        System.out.println("Enter the amount you want to withdraw: ");
+        amt = sc.nextLong();
+        if (balance >= amt) {
+            balance = balance - amt;
+            System.out.println("Balance after withdrawal:\n " + balance);
+        } else {
+            System.out.println("Your balance is less than " + amt + "\tTransaction failed...!!\n");
+        }
+    }
+
+    // method to search an account number
+    public boolean search(String ac_no) {
+        if (accno.equals(ac_no)) {
+            showAccount();
+            return (true);
+        }
+        return (false);
+    }
+
+}
